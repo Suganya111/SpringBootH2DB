6334dff24e2f48c7e933cbae79d48ff2dc8fb185
Babu-b99 <prasathbabu399@gmail.com>
2022-01-18 16:24:29 +0530

    Bank_Acc

---
+++ src/main/java/com/aaludra/spring/jpa/h2/SpringBankApplication.java
@@ -0,0 +1,13 @@
+package com.aaludra.spring.jpa.h2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBankApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/aaludra/spring/jpa/h2/repository/BankDao.java
@@ -0,0 +1,11 @@
+package com.aaludra.spring.jpa.h2.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.aaludra.spring.jpa.h2.model.Bank_Acc;
+
+public interface BankDao extends JpaRepository<Bank_Acc, Long>{
+
+}
